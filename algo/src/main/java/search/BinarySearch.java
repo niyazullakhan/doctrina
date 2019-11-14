e800d918a29f5f3c16e27741b5f6b01a7426fde9
Niyazulla Khan <niyazulla.khan@niyazulla.local>
2019-11-14 12:49:47 +0530

    Binary Search Algo

---
+++ algo/src/main/java/search/BinarySearch.java
@@ -0,0 +1,34 @@
+package search;
+
+/**
+ * Binary search follows divide and conquer strategy
+ * 1) Input needs to be sorted
+ */
+public class BinarySearch {
+    private int search(int[] inputArray, int key, int startPosition, int lastPosition) {
+        if(lastPosition >= startPosition) {
+            int mid = startPosition+(lastPosition - startPosition)/2;
+            if(key == inputArray[mid])
+                return mid;
+            else if(key < inputArray[mid])
+                return search(inputArray, key, startPosition, mid-1);
+            else
+                return search(inputArray, key, mid+1, lastPosition);
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+
+        int[] input = {2, 4, 6, 8, 20};
+
+        int position = binarySearch.search(input, 0, 0, input.length-1);
+        if (position >= 0)
+            System.out.println("found at position: "+position);
+        else
+            System.out.println("Key not found!!");
+
+    }
+}
